489ec905f52dc90a6c70c827131b75021a2ab2f8
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-09-09 12:41:06 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod ea
+        nobis minus, omnis error distinctio doloribus voluptatibus modi,
+        reiciendis eos obcaecati quam animi esse, possimus accusamus blanditiis
+        porro itaque?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias quos
+        veritatis, quidem aut culpa excepturi nihil ex? Sed possimus molestias
+        odio consectetur officia, perferendis natus quas incidunt voluptate
+        consequatur et!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam eaque
+        ab quia nulla reiciendis odit atque nesciunt tempora voluptates
+        cupiditate qui, in dicta delectus officia eos ex. Similique, quo
+        perspiciatis.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
